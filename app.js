a7244a1cf97de38364907a2066046880e8378fad
Manoj-Arutla <manojarutla9@gmail.com>
2026-01-12 12:06:00 +0530

    Making a food delivery app

---
+++ app.js
@@ -0,0 +1,32 @@
+import React  from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.designevo.com/res/templates/thumb_small/location-takeaway-icon.webp"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return(
+        <div className="app">
+            <Header/>
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>)
