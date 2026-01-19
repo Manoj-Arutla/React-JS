862ba031aaf838cb8fc070fc8fba4022376c5c91
Manoj-Arutla <manojarutla9@gmail.com>
2026-01-19 13:15:15 +0530

    Props in React

--- app.js
+++ app.js
@@ -19,10 +19,50 @@ const Header = () => {
     )
 }
 
+
+const RestaurentCard = ({resName, cuisine, rating, deliveryTime}) => { 
+    //this is same as--> const {resName, cuisine, rating, deliveryTime} = props (destructuring)
+    return(
+        <div className="res-card" style = {{backgroundColor: "#f0f0f0"}}>
+            <img className="res-img" 
+            alt="restarentImg" 
+            src="https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/v1704283720/afecca022851255d6eb0b025c9aeaaa3.jpg"/>
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>{rating}</h4>
+            <h4>{deliveryTime}</h4>
+            
+        </div>
+    )
+}
+
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">searchBar</div>
+            <div className="res-container">
+                <RestaurentCard 
+                resName = "Nawab's"
+                cuisine = "Biryani, South Indian"
+                rating = "4.5"
+                deliveryTime = "38 mins"
+                />
+                <RestaurentCard
+                resName = "KFC"
+                cuisine = "Fried Chicken, Burgers"
+                rating = "4.3"
+                deliveryTime = "45 mins"
+                />
+            </div>
+        </div>
+    )
+}
+
 const AppLayout = () => {
     return(
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     )
 }
