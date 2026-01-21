7d4f679eb41fb226a03508ee72d9b2c76ead99d2
Manoj-Arutla <manojarutla9@gmail.com>
2026-01-21 12:08:01 +0530

    default/named imports & exports

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,19 @@
 export const LOGO_URL = "https://www.designevo.com/res/templates/thumb_small/location-takeaway-icon.webp";
 
 export const RESIMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+//exports are two types
+/*
+    1. Default export
+        export default Component;
+    2. Named export
+        export const Component/variable = xyz;
+*/
+
+//similarly imports are two types as well
+/*
+    1. Default import
+        import Component from "path";
+    2. Named import
+        import {Component/variable} from "path";
+*/
