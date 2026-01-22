9056698d2eb417e76f0232827c699df51060c644
Manoj-Arutla <manojarutla9@gmail.com>
2026-01-22 13:00:55 +0530

    useState() hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,42 @@
 import RestaurentCard from "./RestaurentCard";
 import resList from "../utils/resList";
+import { useState } from "react";
 
 const Body = () => {
+    const allRestaurents = resList;
+    let [restaurentList, setRestaurentList] = useState(resList); 
+    //useState() is a react hook which returns a state variable
+    //useState([whatever here]) will be passed to restaurentList
+    //setRestaurentList is called to update the restaurent list
+    //whenever the state variable changes/updates, React re-renders the component
+
+    const filter = () => {
+         const filteredList = allRestaurents.filter((res) => res?.info?.avgRating>4);
+         setRestaurentList(filteredList); //it updates the filtered list into restaurentList
+    }
+
+    const reset = () => {
+        setRestaurentList(allRestaurents);
+    }
+
     return(
         <div className="body">
-            <div className="search">searchBar</div>
+            <div className="filter">
+                <button className="filter-btn"
+                onClick={filter}
+                >
+                Top Rated Restaurents
+                </button>
+
+                <button className="filter-btn"
+                onClick={reset}
+                >
+                All Restaurents
+                </button>
+            </div>
             <div className="res-container">
                 {
-                    resList.map((restaurant) => (<RestaurentCard key={restaurant.info.id} resData={restaurant} />))
+                    restaurentList.map((restaurant) => (<RestaurentCard key={restaurant?.info?.id} resData={restaurant} />))
                     //Using the array index as a key is risky when the list changes dynamically (items are added, removed, or reordered):
                     //but it still is better to use index as key than no key at all
                     //Using the index is acceptable if the list is static and never reordered, deleted, or inserted.
